dfc127fead2494f70afff57a2e60ab1cce0632b4
Michael-Parker719 <scoutmodelmp@gmail.com>
2024-07-15 19:37:24 -0400

    Initial Commit

---
+++ PTCGLeagueManager/PTCGLeagueManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PTCGLeagueManagerApp.swift
+//  PTCGLeagueManager
+//
+//  Created by Michael Parker on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct PTCGLeagueManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
